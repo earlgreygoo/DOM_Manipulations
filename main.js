b13bdb09efc38f5421754653eb3784ee46494cc7
Ryan Meeker <ryanjmeeker@gmail.com>
2016-10-11 15:04:28 -0500

    finished up to question 3

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 
 document.querySelector("#thanks button").addEventListener('click',function(){
   // TASK #1
+  alert("yowch! don't click me so hard")
 })
 
 document.querySelector("#double button").addEventListener('click',function(){
   // TASK #2
+  var pSelect = document.querySelector("#compoundInvestment")
+  var count = pSelect.innerHTML * 2
+  pSelect.innerHTML = count
 })
 
 
 document.querySelector("#color-circle button").addEventListener('click',function(){
   // TASK #3
+  var circle = document.querySelector("circle-bw")
+  var circleCol = circle.background 
+  log
+
 })
 
 document.querySelector("#blow-up button").addEventListener('click',function(){
